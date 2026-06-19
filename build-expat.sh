65ef860176e423758d9ae5f338d63d28799fd72a
Milan Gallo <gallo.milan.jr@gmail.com>
2026-06-19 20:20:14 +0200

    Verbose cmake
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- build-expat.sh
+++ build-expat.sh
@@ -9,7 +9,7 @@ cmake .. \
 	-DBUILD_SHARED_LIBS=OFF \
 	-DCMAKE_INSTALL_PREFIX=../../build \
 	-DCMAKE_BUILD_TYPE=Release && \
-	cmake --build . --config Release && \
+	cmake --build . --config Release --verbose && \
 	cmake --install . --config Release
 cd ../..
 
